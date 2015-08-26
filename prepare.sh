071ce3450f57d3c123f31ce6fc6ca2954934ba46
Conke Hu <conke.hu@maxwit.com>
2015-08-26 19:23:48 +0800

    update
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- prepare.sh
+++ prepare.sh
@@ -1,41 +1,39 @@
 #!/bin/sh
 
-dist=`lsb_release -si`
-
-case "$dist" in
-Ubuntu|Debian)
-	installer="apt-get"
-	;;
-Redhat|Fedora|CentOS|OLinux)
-	installer="yum"
-	;;
-*)
-	echo -e "'$dist' not supported!\n"
-	exit 1
-	;;
-esac
-
-if [ $UID != 0 ]; then
-	installer="sudo $installer"
-fi
-
-which ssh > /dev/null || $installer install -y ssh || exit 1
-# FIXME
-which sshd > /dev/null || $installer install -y openssh-server || exit 1
-which rsync > /dev/null || $installer install -y rsync || exit 1
+#dist=`lsb_release -si`
+#
+#case "$dist" in
+#Ubuntu|Debian)
+#	installer="apt-get"
+#	;;
+#Redhat|Fedora|CentOS|OLinux)
+#	installer="yum"
+#	;;
+#*)
+#	echo -e "'$dist' not supported!\n"
+#	exit 1
+#	;;
+#esac
+#
+#if [ $UID != 0 ]; then
+#	installer="sudo $installer"
+#fi
+#
+#which ssh > /dev/null || $installer install -y ssh || exit 1
+## FIXME
+#which sshd > /dev/null || $installer install -y openssh-server || exit 1
+#which rsync > /dev/null || $installer install -y rsync || exit 1
 
 kt="rsa"
-key="id_$kt"
+kf="id_$kt"
 
-if [ ! -f ~/.ssh/${key} ]; then
-	ssh-keygen -P '' -f ~/.ssh/${key}
+if [ ! -f ~/.ssh/${kf} ]; then
+	ssh-keygen -P '' -f ~/.ssh/${kf}
 fi
-# FIXME
-#if [ ! -f ~/.ssh/authorized_keys ]; then
-  	cp ~/.ssh/${key}.pub ~/.ssh/authorized_keys
-#fi
 
-#export HADOOP\_PREFIX=/usr/local/hadoop
+# FIXME
+key=`cat ~/.ssh/${kf}.pub`
+grep "$key" ~/.ssh/authorized_keys > /dev/null || echo "$key" >> ~/.ssh/authorized_keys
 
 ssh localhost echo "SSH login successfully!" || echo "Fail to login localhost!"
 echo
