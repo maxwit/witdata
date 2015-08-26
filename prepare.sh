5d9973d5d7bb71ab6c51aa70b5e5df44e5da6506
Conke Hu <conke.hu@maxwit.com>
2015-08-27 01:09:25 +0800

    update
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- prepare.sh
+++ prepare.sh
@@ -1,28 +1,10 @@
 #!/bin/sh
 
-#dist=`lsb_release -si`
-#
-#case "$dist" in
-#Ubuntu|Debian)
-#	installer="apt-get"
-#	;;
-#Redhat|Fedora|CentOS|OLinux)
-#	installer="yum"
-#	;;
-#*)
-#	echo -e "'$dist' not supported!\n"
-#	exit 1
-#	;;
-#esac
-#
-#if [ $UID != 0 ]; then
-#	installer="sudo $installer"
-#fi
-#
-#which ssh > /dev/null || $installer install -y ssh || exit 1
-## FIXME
-#which sshd > /dev/null || $installer install -y openssh-server || exit 1
-#which rsync > /dev/null || $installer install -y rsync || exit 1
+if [ $# = 0 ]; then
+	host=localhost
+else
+	host=$1
+fi
 
 kt="rsa"
 kf="id_$kt"
@@ -31,9 +13,15 @@ if [ ! -f ~/.ssh/${kf} ]; then
 	ssh-keygen -P '' -f ~/.ssh/${kf}
 fi
 
-# FIXME
-key=`cat ~/.ssh/${kf}.pub`
-grep "$key" ~/.ssh/authorized_keys > /dev/null || echo "$key" >> ~/.ssh/authorized_keys
+if [ $host = localhost ]; then
+	# FIXME
+	key=`cat ~/.ssh/${kf}.pub`
+	grep "$key" ~/.ssh/authorized_keys > /dev/null || echo "$key" >> ~/.ssh/authorized_keys
+else
+	# FIXME
+	key=`cat ~/.ssh/${kf}.pub`
+	ssh-copy-id $host
+fi
 
-ssh localhost echo "SSH login successfully!" || echo "Fail to login localhost!"
+ssh $host echo "login $host successfully!"
 echo
