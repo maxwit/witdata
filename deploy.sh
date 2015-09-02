c5e2c2c3037f4809aad60cb9cdd95f0189618d05
Conke Hu <conke.hu@maxwit.com>
2015-09-02 13:44:09 +0800

    branch pseudo merged
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- deploy.sh
+++ deploy.sh
@@ -8,32 +8,39 @@ if [ $# != 1 ]; then
 	exit 1
 fi
 
+tarball=$1
+
 if [ -z "$JAVA_HOME" ]; then
 	echo -e "JAVA_HOME not set!\n"
 	exit 1
 fi
 
-if [ ! -e .slaves ]; then
-	echo -e ".slaves does not exist!\n"
-	exit 1
+if [ -e .slaves ]; then
+	mode="cluster"
+else
+	mode="pseudo"
 fi
 
-slaves=(`cat .slaves`)
-if [ ${#slaves[@]} = 0 ]; then
-	echo -e "invalid .slaves!\n"
-	exit 1
+echo -e "configure hadoop as $mode mode!\n"
+
+if [ $mode = "cluster" ]; then
+	slaves=(`cat .slaves`)
+	if [ ${#slaves[@]} = 0 ]; then
+		echo -e "invalid .slaves: no slaves defined!\n"
+		exit 1
+	fi
+
+	# FIXME
+	master=(`ifconfig | grep -o "[1-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"`)
+	master=${master[0]}
+
+	echo "master = $master"
+	echo "slaves = ${slaves[@]}"
+	echo
 fi
 
-tarball=$1
 
-# FIXME
-master=(`ifconfig | grep -o "[1-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*"`)
-master=${master[0]}
-echo "master = $master"
-echo "slaves = ${slaves[@]}"
-echo
-
-hadoop=`basename $1`
+hadoop=`basename $tarball`
 hadoop=${hadoop%%.tar.*}
 
 cd
@@ -51,23 +58,43 @@ sed -i "s:export JAVA_HOME=\${JAVA_HOME}:export JAVA_HOME=${JAVA_HOME}:" etc/had
 bin/hadoop version || exit 1
 echo
 
+### configure sites ###
 cp -v etc/hadoop/mapred-site.xml{.template,}
-patch -p1 < $top/configure-cluster-sites.patch || exit 1
-for xml in core-site hdfs-site mapred-site yarn-site
-do
-	sed -i "s/__MASTER__/$master/g" etc/hadoop/${xml}.xml || exit 1
-done
+patch -p1 < $top/configure-${mode}-sites.patch || exit 1
 
-for slave in ${slaves[@]}
-do
-	scp -r ../$hadoop ${slave}:
-done
+if [ $mode = "cluster" ]; then
+	cp -v .slaves etc/hadoop/slaves	
+
+	for xml in core-site hdfs-site mapred-site yarn-site
+	do
+		sed -i "s/__MASTER__/$master/g" etc/hadoop/${xml}.xml || exit 1
+	done
+
+	# TODO: tar and copy
+	for slave in ${slaves[@]}
+	do
+		scp -r ../$hadoop ${slave}:
+	done
+fi
 
 bin/hdfs namenode -format
 
-sbin/start-all.sh
+#echo "export HADOOP_HOME=\$HOME/$hadoop" >> ~/.bashrc
 
-echo "********************************************"
-echo "    run 'sbin/stop-all.sh to stop hadoop!'"
-echo "********************************************"
+#sbin/start-all.sh
+#echo "********************************************"
+#echo "    run 'sbin/stop-all.sh to stop hadoop!'"
+#echo "********************************************"
+#echo
+
+sbin/start-dfs.sh
+echo "***************************"
+echo "   run 'sbin/stop-dfs.sh'"
+echo "***************************"
+echo
+
+sbin/start-yarn.sh
+echo "***************************"
+echo "   run 'sbin/stop-yarn.sh'"
+echo "***************************"
 echo
