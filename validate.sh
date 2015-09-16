807d0c6067a5f10c1495804e466180d3a8ba8bc3
Conke Hu <conke.hu@maxwit.com>
2015-09-15 20:05:05 -0700

    validate.sh: basic support
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- validate.sh
+++ validate.sh
@@ -2,7 +2,18 @@
 
 temp=`mktemp`
 
-$HOME/hadoop-2.7.1/bin/hadoop fs -put /$temp
+echo "testing 'fs -put' ..."
+$HOME/hadoop-2.7.1/bin/hadoop fs -put $temp /
 
-ssh slave1 $HOME/hadoop-2.7.1/bin/hadoop fs -ls `basename $temp`
-
+for slave in `cat .slaves`
+do
+	echo "@ '$slave' ..."
+	ssh $slave $HOME/hadoop-2.7.1/bin/hadoop fs -ls /`basename $temp`
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		echo "failed."
+		exit $ret
+	else
+		echo "OK."
+	fi
+done
