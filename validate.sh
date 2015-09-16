946569c4f5faad08f3e8d918b0bf8da2fbba5cd4
Conke Hu <conke.hu@maxwit.com>
2015-09-16 06:54:03 -0700

    validate.sh: update prompt
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- validate.sh
+++ validate.sh
@@ -3,17 +3,19 @@
 . ./parse-config.sh
 
 temp=`ssh $user@$master mktemp`
-ssh $user@$master hadoop-2.7.1/bin/hadoop fs -put $temp /
+
+echo "putting $temp to $user@$master ..."
+ssh $user@$master hadoop-2.7.1/bin/hadoop fs -put $temp / || exit 1
 
 for slave in ${slaves[@]}
 do
-	echo "@ '$slave' ..."
-	ssh $user@$slave hadoop-2.7.1/bin/hadoop fs -ls /`basename $temp`
-	ret=$?
-	if [ $ret -ne 0 ]; then
-		echo "Failed."
-		exit $ret
-	else
-		echo "OK."
-	fi
+	echo "checking '$slave' ..."
+	ssh $user@$slave hadoop-2.7.1/bin/hadoop fs -ls /`basename $temp` || exit 1
+#	ret=$?
+#	if [ $ret -ne 0 ]; then
+#		echo "Failed."
+#		exit $ret
+#	else
+#		echo "OK."
+#	fi
 done
