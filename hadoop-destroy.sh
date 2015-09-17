a9cb5a704ee3dfbc048295a9af2f7b290dce39fd
Conke Hu <conke.hu@maxwit.com>
2015-09-17 16:00:53 +0800

    update destroy.sh for ci testing
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- hadoop-destroy.sh
+++ hadoop-destroy.sh
@@ -4,6 +4,9 @@ master=`hostname`
 
 [ -z "$HADOOP_HOME" ] && exit 0
 
+# FIXME
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
 if [ -d $HADOOP_HOME ]; then
 	$HADOOP_HOME/sbin/stop-dfs.sh || exit 1
 	$HADOOP_HOME/sbin/stop-yarn.sh || exit 1
@@ -11,13 +14,15 @@ if [ -d $HADOOP_HOME ]; then
 	for slave in localhost `cat $HADOOP_HOME/etc/hadoop/slaves`
 	do
 		echo "removing $HADOOP_HOME @ $slave"
-		if [ $slave = 'localhost' ]; then
-			ssh_cmd=""
-		else
-			ssh_cmd="ssh $slave"
-		fi
-		$ssh_cmd rm -rf $HADOOP_HOME || exit 1
-		$ssh_cmd rm -rf /tmp/hadoop-$USER || exit 1
+		sshh $slave rm -rf $HADOOP_HOME || exit 1
+		sshh $slave rm -rf /tmp/hadoop-$USER || exit 1
+		#if [ $slave = 'localhost' ]; then
+		#	ssh_cmd=""
+		#else
+		#	ssh_cmd="ssh $slave"
+		#fi
+		#$ssh_cmd rm -rf $HADOOP_HOME || exit 1
+		#$ssh_cmd rm -rf /tmp/hadoop-$USER || exit 1
 	done
 fi
 
