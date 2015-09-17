7f64688da7a0c4a2563a1b0a49d87deb6f1e9088
Conke Hu <conke.hu@maxwit.com>
2015-09-17 15:21:20 +0800

    push for ci testing
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- hadoop-destroy.sh
+++ hadoop-destroy.sh
@@ -11,8 +11,13 @@ if [ -d $HADOOP_HOME ]; then
 	for slave in localhost `cat $HADOOP_HOME/etc/hadoop/slaves`
 	do
 		echo "removing $HADOOP_HOME @ $slave"
-		ssh $slave rm -rf $HADOOP_HOME || exit 1
-		ssh $slave rm -rvf /tmp/hadoop-$USER || exit 1
+		if [ $slave = 'localhost' ]; then
+			ssh_cmd=""
+		else
+			ssh_cmd="ssh $slave"
+		fi
+		$ssh_cmd rm -rf $HADOOP_HOME || exit 1
+		$ssh_cmd rm -rf /tmp/hadoop-$USER || exit 1
 	done
 fi
 
