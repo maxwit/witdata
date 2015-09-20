008336a08f510a9cfa60f740c2ef821819844afd
Conke Hu <conke.hu@maxwit.com>
2015-09-20 10:23:11 -0700

    ci-test.sh: support normal branches
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- ci-test.sh
+++ ci-test.sh
@@ -7,19 +7,23 @@ else
 fi
 pusher=`echo $BRANCH | awk -F '-' '{print $2}'`
 
-if [ -z "$pusher" ]; then
-	echo "branch not detected!"
-	exit 1
+if [ ! -z "$pusher" ]; then
+	master="$pusher-node1.maxwit.com"
+	slaves="$pusher-node2.maxwit.com $pusher-node3.maxwit.com"
+else
+	# FIXME
+	master=`hostname`
+	slaves="${master//1/2} ${master//1/3}"
 fi
 
 cat > .config << EOF
 [hadoop]
-master = $pusher-node1.maxwit.com
-slaves = $pusher-node2.maxwit.com $pusher-node3.maxwit.com
+master = $master
+slaves = $slaves
 
 [hive]
 
 [zookeeper]
 EOF
 
-./remote-deploy.sh -u hadoop -m $pusher-node1.maxwit.com || exit 1
+./remote-deploy.sh -u hadoop -m $master || exit 1
