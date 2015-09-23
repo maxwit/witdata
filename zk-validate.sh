c62bf26eee332469b381dfbf6c0e003c7611711f
Conke Hu <conke.hu@maxwit.com>
2015-09-23 16:55:53 +0800

    push for ci testing
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- zk-validate.sh
+++ zk-validate.sh
@@ -14,12 +14,10 @@ echo
 
 cd $ZOOKEEPER_HOME || exit 1
 
-pwd
-ls
-ls bin/
 bin/zkCli.sh -server 127.0.0.1:2181 << EOF
 create /zk_demo demo1
 ls /
+get /zk_demo
 set /zk_demo demo2
 get /zk_demo
 EOF
