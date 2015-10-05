a5b1dd3cb50cfb7f73b0a8ab4732bdd3f5d7b534
Conke Hu <conke.hu@maxwit.com>
2015-10-05 08:30:16 -0700

    disable zk
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- ci-test.sh
+++ ci-test.sh
@@ -23,7 +23,7 @@ config_slaves="$slaves"
 
 #config_hadoop="hadoop-2.7.1"
 #config_hive="apache-hive-1.2.1-bin"
-config_zk="zookeeper-3.4.6"
+#config_zk="zookeeper-3.4.6"
 config_hbase="hbase-1.1.2"
 EOF
 
