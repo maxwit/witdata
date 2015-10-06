bc95c2471cd3821462f1e5f8206f4c9548946709
Conke Hu <conke.hu@maxwit.com>
2015-10-05 23:57:06 -0700

    update hadoop.sh
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- hadoop.sh
+++ hadoop.sh
@@ -63,21 +63,16 @@ function hadoop_destroy
 	if [ -d $HADOOP_HOME ]; then
 		$HADOOP_HOME/sbin/stop-dfs.sh || exit 1
 		$HADOOP_HOME/sbin/stop-yarn.sh || exit 1
-
-		for slave in localhost `cat $HADOOP_HOME/etc/hadoop/slaves`
-		do
-			echo "removing $HADOOP_HOME @ $slave"
-			ssh $slave rm -rf $HADOOP_HOME || exit 1
-			ssh $slave rm -rf /tmp/hadoop-$USER || exit 1
-			#if [ $slave = 'localhost' ]; then
-			#	ssh_cmd=""
-			#else
-			#	ssh_cmd="ssh $slave"
-			#fi
-			#$ssh_cmd rm -rf $HADOOP_HOME || exit 1
-			#$ssh_cmd rm -rf /tmp/hadoop-$USER || exit 1
-		done
 	fi
+
+	for host in localhost `cat $HADOOP_HOME/etc/hadoop/slaves`
+	do
+		echo "removing $HADOOP_HOME @ $host"
+		ssh $host << EOF
+rm -rf $HADOOP_HOME
+rm -rf /tmp/hadoop-$USER
+EOF
+	done
 
 	del_export HADOOP_HOME
 	# echo "'export HADOOP_HOME' removed from $sh_config" $sh_config
